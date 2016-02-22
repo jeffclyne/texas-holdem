834333023d5d7bdb72453e4785ee343c544af3d1
Jeff Clyne <jefe.wyatt@gmail.com>
2016-02-22 15:18:28 -0800

    adding tests and fixes

--- test/index.js
+++ test/index.js
@@ -4,12 +4,38 @@ import chai from 'chai';
 const should = chai.should();
 
 describe('texas-holdem', () => {
-  it('should deal hands to all players', () => {
-    holdem.addPlayer('Player 1');
-    holdem.addPlayer('Player 2');
-    holdem.dealHand();
+  let engine = new holdem.Engine();
+  afterEach(() => {
+    engine = new holdem.Engine();
+  });
 
-    holdem.players[0].hand.length.should.equal(2);
-    holdem.players[1].hand.length.should.equal(2);
+  it('should deal hands to all players', () => {
+    engine.addPlayer('Player 1');
+    engine.addPlayer('Player 2');
+    engine.dealHand();
+    engine.players[0].hand.length.should.equal(2);
+    engine.players[1].hand.length.should.equal(2);
+  });
+
+  it('should correctly add the blinds', () => {
+    engine.addPlayer('Player 1');
+    engine.addPlayer('Player 2');
+    engine.addPlayer('Player 3');
+    engine.dealHand();
+
+    engine.players[0].currentBet.should.equal(0);
+    engine.players[1].currentBet.should.equal(5);
+    engine.players[2].currentBet.should.equal(10);
+    engine.pot.should.equal(15);
+    engine.currentBet.should.equal(10);
+  });
+
+  it('should set the active player', () => {
+    engine.addPlayer('Player 1');
+    engine.addPlayer('Player 2');
+    engine.addPlayer('Player 3');
+    engine.dealHand();
+
+    engine.activePlayer.name.should.equal('Player 1');
   });
 });
