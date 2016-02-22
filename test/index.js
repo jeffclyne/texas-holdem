46899d1e20fb6e0f3e3f3f89bbe90901d605dd34
Jeff Clyne <jefe.wyatt@gmail.com>
2016-02-22 13:51:11 -0800

    make test pass

--- test/index.js
+++ test/index.js
@@ -1,12 +1,15 @@
 import assert from 'assert';
 import holdem from '../lib/holdem';
+import chai from 'chai';
+const should = chai.should();
 
 describe('texas-holdem', () => {
   it('should deal hands to all players', () => {
     holdem.addPlayer('Player 1');
     holdem.addPlayer('Player 2');
+    holdem.dealHand();
 
-    assert(holdem.players[0].hand.length === 2);
-    assert(holdem.players[1].hand.length === 2);
+    holdem.players[0].hand.length.should.equal(2);
+    holdem.players[1].hand.length.should.equal(2);
   });
 });
