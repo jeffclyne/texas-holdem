3dbfcb1cd1e2bca1c5e22ae2735350fc6cba2df3
Jeff Clyne <jefe.wyatt@gmail.com>
2016-02-22 13:40:57 -0800

    adding a mocha test

--- test/index.js
+++ test/index.js
@@ -1,8 +1,12 @@
 import assert from 'assert';
-import texasHoldem from '../lib';
+import holdem from '../lib/holdem';
 
-describe('texas-holdem', function () {
-  it('should have unit test!', function () {
-    assert(false, 'we expected this package author to add actual unit tests.');
+describe('texas-holdem', () => {
+  it('should deal hands to all players', () => {
+    holdem.addPlayer('Player 1');
+    holdem.addPlayer('Player 2');
+
+    assert(holdem.players[0].hand.length === 2);
+    assert(holdem.players[1].hand.length === 2);
   });
 });
